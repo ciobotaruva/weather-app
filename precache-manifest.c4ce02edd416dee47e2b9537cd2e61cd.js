0930d435de32713d2fe2dbbdfb541608e6acad8a
Ciobotaru Valentin <Ciobotaruva@gmail.com>
2020-08-30 21:21:37 +0300

    Updates

---
+++ precache-manifest.c4ce02edd416dee47e2b9537cd2e61cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f6d94cc874467d65bbd30ebc6e6c51",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a4f10efdf89fa0023a00",
+    "url": "/weather-app/static/css/main.562b5cbc.chunk.css"
+  },
+  {
+    "revision": "7f0e1bd57fff2ec8ceb2",
+    "url": "/weather-app/static/js/2.23ab1519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.23ab1519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f10efdf89fa0023a00",
+    "url": "/weather-app/static/js/main.0f422af9.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
